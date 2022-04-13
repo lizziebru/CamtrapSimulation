b1571c61b302a696ebf9b0370b86c42e0799a45f
lizziebru <e.bru21@imperial.ac.uk>
2022-04-13 19:07:54 +0100

    Make script to compile multiple runs of the same simulation

---
+++ code/simulation/compile.R
@@ -0,0 +1,10 @@
+### compile data on multiple runs of the same simulation ###
+
+setwd("~/Documents/Project/CamtrapSimulation/code/simulation")
+
+setwd("results1/0.05_5e5_0_0,40/1_0.5_0.5") # set which one to analyse
+
+# load in data:
+load("seq_dats.RData")
+
+## to do: do this once have more runs finished tomorrow

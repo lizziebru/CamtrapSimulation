7c92aacbafe47b1b283a33cdb9c51cf0e2b0a1dc
Lizzie Bru <e.bru21@imperial.ac.uk>
2022-07-24 13:48:24 +0100

    Incorporate pipeline script for other computers into local pipeline one
    Not really needed anymore anyways

--- code/pipeline_local_seqdats.R
+++ code/pipeline_local_seqdats.R
@@ -33,3 +33,38 @@ generate_seqdats(parentfolder=parentfolder, Mb_range, path_nos=path_nos, r=r, th
 # # run the simulation and analyse the results:
 # run_and_analyse(parentfolder=parentfolder, pathfolder=pathfolder, iter=iter, species=species, r=r, th=th, twoCTs=twoCTs, connectedCTs=connectedCTs, n_cores=4)
 
+
+
+
+
+# for running this on other computers: -------------------------------------
+
+# ## FOR RUNNING THE SIMULATION ON OTHER COMPUTERS ##
+# 
+# # required packages and functions
+# 
+# #install.packages("ggplot2")
+# require(ggplot2)
+# 
+# #install.packages("circular")
+# require(circular)
+# 
+# # install.packages("parallel")
+# # require(parallel)
+# 
+# source("CamtrapSimulation.R", echo=TRUE)
+# 
+# parentfolder <- paste0("")
+# pathfolder <- paste0("")
+# 
+# iter <- 100 
+# 
+# species = 0 
+# r = 9
+# th = 0.7
+# path_cutby = 0.5
+# twoCTs = TRUE
+# connectedCTs = FALSE
+# 
+# run_and_analyse(parentfolder=parentfolder, pathfolder=pathfolder, iter=iter, species=species, r=r, th=th, twoCTs=twoCTs, connectedCTs=connectedCTs, path_cutby = path_cutby, n_cores=4)
+

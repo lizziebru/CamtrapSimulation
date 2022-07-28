9c93d0c9968e745437885c0439affe00065d0e6f
Lizzie Bru <e.bru21@imperial.ac.uk>
2022-07-28 17:51:45 +0100

    Add in line to change name of iter0 to iter20

--- code/rename_path_results.sh
+++ code/rename_path_results.sh
@@ -18,20 +18,7 @@ do
 	mv ${files[i]} iter${i}.RData
 done
 
-<<<<<<< HEAD
-
-#rm *.tgz   # learnt from mistakes: don't do this until you've checked that the untarred files are ok
-
-#counter=0
-#for file in *.RData; do
-#counter=counter+1
-#    mv "$file" "iter${counter}.RData"
-#done
-=======
 mv iter0.RData iter20.RData # bc bash indexes from 0 but I prefer indexing from 1
-
->>>>>>> master
-
 
 
 
